cecbbcebe12c78da2e649d06fc350746be449877
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-10 13:59:00 -0300

    Created enums

---
+++ src/utils/dicts/enumPlayerPositionBase.ts
@@ -0,0 +1,7 @@
+export enum EnumPlayerPositionBase {
+  Goalkeeper = "Goalkeeper",
+  Defenser = "Defenser",
+  Midfielder = "Midfielder",
+  Winger = "Winger",
+  Striker = "Striker",
+}
---
+++ src/utils/dicts/enumRoleType.ts
@@ -0,0 +1,8 @@
+export enum EnumRoleType {
+  Attack = "Attack",
+  Support = "Support",
+  Defend = "Defend",
+  Stopper = "Stopper",
+  Cover = "Cover",
+  Automatic = "Automatic",
+}
