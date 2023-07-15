ce50ce6b964537ba599e7e163977897ab4f2aa21
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-15 13:57:16 -0300

    Implemented delete team logo when delete team register

--- src/services/team/delete/deleteTeamService.ts
+++ src/services/team/delete/deleteTeamService.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import fs from "fs/promises";
 import { TeamsRepository } from "./../../../repositories/teamsRepository";
 
 interface DeleteTeamRequest {
@@ -13,6 +15,26 @@ export class DeleteTeamService {
     const verifyIndex = await this.teamsRepository.findIndex(id);
 
     if (verifyIndex < 0) throw new Error("Team not found!");
+
+    const team = await this.teamsRepository.findById(id);
+
+    const teamLogo = team?.teamLogo;
+
+    if (!teamLogo) {
+      throw new Error("Team logo not found!");
+    }
+
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+    const logoPath = path.join(uploadsFolder, teamLogo);
+
+    await fs.unlink(logoPath);
 
     await this.teamsRepository.delete(id);
 
