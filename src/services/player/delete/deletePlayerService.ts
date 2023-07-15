896c19173156b0f171213a6e002cd1653b59214e
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-15 14:05:51 -0300

    Implemented delete player image when delete player register

--- src/services/player/delete/deletePlayerService.ts
+++ src/services/player/delete/deletePlayerService.ts
@@ -1,3 +1,5 @@
+import fs from "fs/promises";
+import path from "path";
 import { PlayersRepository } from "./../../../repositories/playersRepository";
 
 interface DeletePlayerRequest {
@@ -13,6 +15,26 @@ export class DeletePlayerService {
     const playerIndex = await this.playersRepository.findIndex(id);
 
     if (playerIndex < 0) throw new Error("Player not found!");
+
+    const player = await this.playersRepository.findById(id);
+
+    const playerImage = player?.playerImage;
+
+    if (!playerImage) {
+      throw new Error("Team logo not found!");
+    }
+
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+    const logoPath = path.join(uploadsFolder, playerImage);
+
+    await fs.unlink(logoPath);
 
     this.playersRepository.delete(id);
 
