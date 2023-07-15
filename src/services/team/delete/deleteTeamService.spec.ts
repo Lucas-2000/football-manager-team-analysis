7e3c9000fda1c7bb2f8ba01c2474c0dd3ecd908c
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-15 14:13:00 -0300

    Fixed delete team service test

--- src/services/team/delete/deleteTeamService.spec.ts
+++ src/services/team/delete/deleteTeamService.spec.ts
@@ -1,14 +1,18 @@
+import fs from "fs/promises";
+import path from "path";
 import { describe, expect, it } from "vitest";
 import { InMemoryTeamsRepository } from "../../../repositories/inMemory/inMemoryTeamsRepository";
 import { CreateTeamService } from "../create/createTeamService";
 import { DeleteTeamService } from "./deleteTeamService";
 import { EnumTeamGrade } from "@prisma/client";
+import { UploadTeamLogoService } from "../uploadLogo/uploadTeamLogoService";
 
 describe("Delete Team Service", () => {
   it("should be able to delete a team", async () => {
     const teamsRepository = new InMemoryTeamsRepository();
     const createTeam = new CreateTeamService(teamsRepository);
     const deleteTeam = new DeleteTeamService(teamsRepository);
+    const uploadLogoTeam = new UploadTeamLogoService(teamsRepository);
 
     await createTeam.execute({
       id: "1",
@@ -17,6 +21,24 @@ describe("Delete Team Service", () => {
       teamCountry: "Brasil",
       teamLeague: "Brasileirão",
       teamGrade: EnumTeamGrade.A,
+    });
+
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+    const tempFilePath = path.join(uploadsFolder, "teste");
+
+    // Cria um arquivo temporário
+    fs.writeFile(tempFilePath, "conteúdo do arquivo");
+
+    await uploadLogoTeam.execute({
+      id: "1",
+      teamLogo: "teste",
     });
 
     await expect(
