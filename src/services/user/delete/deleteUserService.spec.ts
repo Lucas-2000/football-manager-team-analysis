be632e0e39bcffb03fd4d9f985c8039a7b99c195
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-17 14:37:46 -0300

    Fixed uploads on delete user service

--- src/services/user/delete/deleteUserService.spec.ts
+++ src/services/user/delete/deleteUserService.spec.ts
@@ -1,7 +1,10 @@
+import fs from "fs/promises";
+import { UploadUserAvatarService } from "./../uploadAvatar/uploadUserAvatarService";
 import { describe, expect, it } from "vitest";
 import { InMemoryUsersRepository } from "../../../repositories/inMemory/inMemoryUsersRepository";
 import { CreateUserService } from "../create/createUserService";
 import { DeleteUserService } from "./deleteUserService";
+import path from "path";
 
 describe("Delete User Service", () => {
   it("should be able to delete a user", async () => {
@@ -15,6 +18,45 @@ describe("Delete User Service", () => {
       email: "test@example.com",
       password: "test123",
       avatar: null,
+    });
+
+    await expect(
+      deleteUserService.execute({ id: "1" })
+    ).resolves.toBeInstanceOf(Array);
+  });
+
+  it("should be able to delete a user and avatar from uploas folder if user have avatar", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const createUserService = new CreateUserService(usersRepository);
+    const deleteUserService = new DeleteUserService(usersRepository);
+    const uploadUserAvatarService = new UploadUserAvatarService(
+      usersRepository
+    );
+
+    await createUserService.execute({
+      id: "1",
+      username: "test",
+      email: "test@example.com",
+      password: "test123",
+      avatar: null,
+    });
+
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+    const tempFilePath = path.join(uploadsFolder, "teste");
+
+    // Cria um arquivo temporário
+    await fs.writeFile(tempFilePath, "conteúdo do arquivo");
+
+    await uploadUserAvatarService.execute({
+      id: "1",
+      avatar: "teste",
     });
 
     await expect(
--- src/services/user/delete/deleteUserService.ts
+++ src/services/user/delete/deleteUserService.ts
@@ -1,3 +1,5 @@
+import fs from "fs/promises";
+import path from "path";
 import { UsersRepository } from "../../../repositories/usersRepository";
 
 interface DeleteUserServiceRequest {
@@ -15,6 +17,24 @@ export class DeleteUserService {
     const verifyIndex = await this.usersRepository.findIndex(id);
 
     if (verifyIndex < 0) throw new Error("Team not found!");
+
+    const user = await this.usersRepository.findById(id);
+
+    const avatar = user?.avatar;
+
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+
+    if (avatar) {
+      const logoPath = path.join(uploadsFolder, avatar);
+      await fs.unlink(logoPath);
+    }
 
     await this.usersRepository.delete(id);
 
