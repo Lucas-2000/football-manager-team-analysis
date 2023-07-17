d2bf1b65064e5d70d9bf10f4a24e9e94da376b7e
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-17 11:37:32 -0300

    Implemented find user by id methods

---
+++ src/services/user/findById/findUserByIdService.spec.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "vitest";
+import { InMemoryUsersRepository } from "../../../repositories/inMemory/inMemoryUsersRepository";
+import { FindUserByIdService } from "./findUserByIdService";
+import { CreateUserService } from "../create/createUserService";
+
+describe("Find User By Id", () => {
+  it("should be able to find a user by id", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const findUserByIdService = new FindUserByIdService(usersRepository);
+    const createUserService = new CreateUserService(usersRepository);
+
+    await createUserService.execute({
+      id: "1",
+      username: "test",
+      email: "test@example.com",
+      password: "test123",
+      avatar: null,
+    });
+
+    expect(findUserByIdService.execute({ id: "1" })).resolves.toHaveProperty(
+      "id"
+    );
+  });
+
+  it("should not be able to find a user by id if user not found", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const findUserByIdService = new FindUserByIdService(usersRepository);
+    const createUserService = new CreateUserService(usersRepository);
+
+    await createUserService.execute({
+      id: "1",
+      username: "test",
+      email: "test@example.com",
+      password: "test123",
+      avatar: null,
+    });
+
+    expect(findUserByIdService.execute({ id: "2" })).rejects.toBeInstanceOf(
+      Error
+    );
+  });
+});
