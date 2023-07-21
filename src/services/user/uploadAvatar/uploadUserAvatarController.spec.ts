20df9ca850e61e7118f5d29f9ec7030fa82437e7
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-21 00:12:44 -0300

    Implemented upload user avatar integration test

---
+++ src/services/user/uploadAvatar/uploadUserAvatarController.spec.ts
@@ -0,0 +1,49 @@
+import fs from "fs/promises";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "../../../app";
+import path from "path";
+import { DeleteUserService } from "../delete/deleteUserService";
+import { PrismaUsersRepository } from "../../../repositories/prisma/prismaUsersRepository";
+
+describe("Upload user avatar controller", () => {
+  let user: request.Response;
+
+  beforeEach(async () => {
+    user = await request(app).post("/users").send({
+      username: "test-integration-user-avatar",
+      email: "test-integration-user-avatar@example.com",
+      password: "test123",
+    });
+  });
+
+  it("should be able to update user avatar", async () => {
+    const uploadsFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "uploads"
+    ); // Caminho base da pasta de uploads
+    const tempFilePath = path.join(uploadsFolder, "teste");
+
+    // Cria um arquivo temporário
+    await fs.writeFile(tempFilePath, "conteúdo do arquivo");
+
+    const response = await request(app)
+      .post(`/users/${user.body.id}/avatar`)
+      .attach("file", tempFilePath);
+
+    expect(response.status).toBe(201);
+  });
+
+  afterEach(async () => {
+    const usersRepository = new PrismaUsersRepository();
+    const deleteUserService = new DeleteUserService(usersRepository);
+
+    await deleteUserService.execute({
+      id: user.body.id,
+    });
+  });
+});
