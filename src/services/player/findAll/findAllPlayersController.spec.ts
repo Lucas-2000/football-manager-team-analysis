99d9ed16fd532ad03583ef21695c43cb1c51f239
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-07-23 19:41:34 -0300

    Implemented find all players integration tests

---
+++ src/services/player/findAll/findAllPlayersController.spec.ts
@@ -0,0 +1,33 @@
+import { afterEach, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "../../../app";
+import { prisma } from "../../../utils/config/prisma/prismaClient";
+
+describe("Find All Players Controller", () => {
+  it("should be able to find all players", async () => {
+    await request(app).post("/users").send({
+      username: "test-integration-find-all-players",
+      email: "test-integration-find-all-players@example.com",
+      password: "test123",
+    });
+
+    const req = await request(app).post("/users/auth").send({
+      username: "test-integration-find-all-players",
+      password: "test123",
+    });
+
+    const response = await request(app)
+      .get("/players")
+      .set("Authorization", `Bearer ${req.body.token}`);
+
+    expect(response.status).toBe(201);
+  });
+
+  afterEach(async () => {
+    await prisma.user.delete({
+      where: {
+        email: "test-integration-find-all-players@example.com",
+      },
+    });
+  });
+});
